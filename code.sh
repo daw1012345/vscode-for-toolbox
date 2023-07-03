ffbf5c6561324510fa9f4ee5b1a1f0e0a851ad68
Dawid Kulikowski <git@dawidkulikowski.pl>
2023-07-03 14:12:50 +0200

    Use an installation script to allow offline use

--- code.sh
+++ code.sh
@@ -13,15 +13,12 @@ if [ ! -f "/run/.containerenv" ]; then
      exit
 fi
 
-VSCODE_GLOBAL_CONFIG="$HOME/.var/app/com.visualstudio.code/config/Code/User/settings.json"
-
 CONTAINER_NAME="$(source /run/.containerenv && echo $name)"
 CONTAINER_ID="$(source /run/.containerenv && echo $id)"
 # Copied from toolbox-vscode because xxd is not installed by default
 CONTAINER_ID_ENC=$(echo -n "$CONTAINER_ID" | od -t x1 -A none -v | tr -d ' \n')
 
 CONFIG_FILE="$HOME/.var/app/com.visualstudio.code/config/Code/User/globalStorage/ms-vscode-remote.remote-containers/nameConfigs/$CONTAINER_NAME.json"
-
 
 # By default, VSCode will try to do everything as root messing up permissions. Force it to not do that
 if [ ! -f "$CONFIG_FILE" ]; then
@@ -36,31 +33,6 @@ if [ -f "$CONFIG_FILE" ] && [ ! 'grep -q "remoteUser" "$CONFIG_FILE"' ]; then
      echo "[!!!!!!] You already have a configuration file for this container but the remoteUser option is missing. No changes will be made, but you may experience issues with permissions."
 fi
 
-# Install extension that will allow VScode to access podman from the flatpak
-flatpak-spawn --host flatpak install com.visualstudio.code.tool.podman//22.08/ --noninteractive -y
-
-# Allow vscode to reach podman from within the sandbox
-flatpak-spawn --host flatpak override --user --filesystem=xdg-run/podman com.visualstudio.code
-
-if [ ! -f "$VSCODE_GLOBAL_CONFIG" ]; then
-     mkdir -p "$(dirname "$VSCODE_GLOBAL_CONFIG")"
-     echo "{}" > "$VSCODE_GLOBAL_CONFIG"
-fi
-
-# Update vscode configuration to make use of podman instead of docker (toolbox uses podman and the two are interchangable)
-# At first glance this may seem weird, but using sed to write JSON is finicky at best and jq is not installed on Fedora by default. AFAIK python should at least be available on the host
-flatpak-spawn --host python3 <<EOF
-import json
-
-with open("$VSCODE_GLOBAL_CONFIG", 'r+') as fd:
-     data = json.loads(fd.read())
-     data['dev.containers.dockerPath'] = '/app/tools/podman/bin/podman-remote'
-     data['dev.containers.dockerComposePath'] = 'podman-compose'
-     fd.seek(0)
-     fd.truncate()
-     fd.write(json.dumps(data))
-EOF
-
 CTR_ENV="$(flatpak-spawn --host podman inspect ${CONTAINER_ID} --format='{{.Config.Env}}')"
 if ! echo "$CTR_ENV" | grep -q "HOME=$HOME" ;then
      echo "[-] Incorrect HOME env is set, this can be (temporairly) solved by running the following on your **host**:"
@@ -72,9 +44,6 @@ rm -rf "$HOME/.vscode-server"
 sudo mkdir -p /opt/vscode-server/
 sudo chmod a+rwx /opt/vscode-server/
 ln -T -sf /opt/vscode-server/ "$HOME/.vscode-server" 
-
-# Actually make sure that we have the remote containers vscode extension
-flatpak-spawn --host flatpak run com.visualstudio.code --install-extension ms-vscode-remote.remote-containers
 
 echo "[+] Launching VScode..."
 
---
+++ install.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import json
+import os
+import shutil
+
+"""Tell VSCode to use Podman instead of Docker"""
+def write_vscode_config():
+    print("Telling VSCode to use podman instead of Docker")
+    global_config_path = os.path.expanduser('~/.var/app/com.visualstudio.code/config/Code/User/settings.json')
+    with open(global_config_path, 'r+') as fd:
+        try:
+            data = json.loads(fd.read())
+        except ValueError:
+            data = dict()
+
+        data['dev.containers.dockerPath'] = '/app/tools/podman/bin/podman-remote'
+        data['dev.containers.dockerComposePath'] = 'podman-compose'
+        fd.seek(0)
+        fd.truncate()
+        fd.write(json.dumps(data))
+
+"""Install extension allowing containerized VSCode to use Podman on the host and the dev containers extension"""
+def install_portal():
+    print("Installing VSCode -> Podman portal")
+    os.system("flatpak install com.visualstudio.code.tool.podman//22.08/ --noninteractive -y")
+    print("Allowing VSCode to use Podman")
+    os.system("flatpak override --user --filesystem=xdg-run/podman com.visualstudio.code")
+    print("Installing the Remote Containers VSCode extension")
+    os.system("flatpak run com.visualstudio.code --install-extension ms-vscode-remote.remote-containers")
+
+def install_integration():
+    print("Installing helper...")
+    bin_path = os.path.expanduser("~/.local/bin")
+
+    if not os.path.exists(bin_path):
+         os.makedirs(bin_path)
+    shutil.copyfile("code.sh", os.path.expanduser("~/.local/bin/code"))
+    
+
+if __name__ == "__main__":
+    install_portal()
+    write_vscode_config()
+    install_integration()
